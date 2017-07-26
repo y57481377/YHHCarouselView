062f2c8c7bca3d42fe20d38100dd2e7c0f8bbd1b
y578471377 <578471377@qq.com>
2017-07-26 10:29:56 +0800

    Initial Commit

---
+++ CarouselView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarouselView
+//
+//  Created by Yang on 2017/7/26.
+//  Copyright © 2017年 YHH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CarouselView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CarouselView
+//
+//  Created by Yang on 2017/7/26.
+//  Copyright © 2017年 YHH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
